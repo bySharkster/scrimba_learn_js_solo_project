9cfa64c52ea5d26fa1c74888c60787fcd206f11f
bySharkster <contact@fernandoaponte.dev>
2025-12-18 14:54:13 -0500

    feat: added winning highlighting

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 let homeScore = 0;
 let guestScore = 0;
 
-let homeScoreParagraph = document.getElementById("home-score");
-let guestScoreParagraph = document.getElementById("guest-score");
+let homeScoreParagraph = document.getElementById('home-score');
+let guestScoreParagraph = document.getElementById('guest-score');
 
 function addPoints(scoreTeam, points) {
+    if (scoreTeam === 'homeScore') {
+        homeScore += points;
+    } else if (scoreTeam === 'guestScore') {
+        guestScore += points;
+    } else {
+        console.log('Invalid team');
+    }
 
- if (scoreTeam === "homeScore") {
-    homeScore += points;
- } else if (scoreTeam === "guestScore") {
-    guestScore += points;
- } else {
-    console.log("Invalid team");
- }
- 
- homeScoreParagraph.textContent = homeScore;
- guestScoreParagraph.textContent = guestScore;
+    homeScoreParagraph.textContent = homeScore;
+    guestScoreParagraph.textContent = guestScore;
 
+    winningTeam();
 }
 
 // add new game button (reset scores)
-function newGame(){
+function newGame() {
     homeScore = 0;
     guestScore = 0;
     homeScoreParagraph.textContent = homeScore;
     guestScoreParagraph.textContent = guestScore;
+    homeScoreParagraph.classList.remove('winning');
+    guestScoreParagraph.classList.remove('winning');
 }
 
 // highlight team with higher score
 
-// add a few more counters 
+function winningTeam() {
+    let winningTeam = homeScore > guestScore ? 'home' : homeScore === guestScore ? 'draw' : 'guest';
+
+    if (winningTeam === 'home') {
+        guestScoreParagraph.classList.remove('winning');
+        homeScoreParagraph.classList.add('winning');
+    } else if (winningTeam === 'guest') {
+        homeScoreParagraph.classList.remove('winning');
+        guestScoreParagraph.classList.add('winning');
+    } else {
+        homeScoreParagraph.classList.add('winning');
+        guestScoreParagraph.classList.add('winning');
+    }
+}
+// add a few more counters
 // period, fouls, timer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
